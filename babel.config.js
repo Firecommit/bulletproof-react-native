ea2404da1dcbde00b9d85f85795ba101084638fc
maturu <maturu.410@gmail.com>
2022-04-23 06:56:54 +0900

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': path.resolve(__dirname, 'src'),
+        },
+      },
+      'module:react-native-dotenv',
+      {
+        moduleName: '$env',
+        path: './.env',
+      },
+    ],
+  ],
+};
